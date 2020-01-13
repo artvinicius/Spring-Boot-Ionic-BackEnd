64c483e0c98862ca949f6e24f01e39644eb78573
ArthurViniciusx <arthurviny1999@gmail.com>
2020-01-13 14:06:51 -0300

    Criando Operação de instanciação

--- src/main/java/com/arthurvinicius/cursomc/CursomApplication.java
+++ src/main/java/com/arthurvinicius/cursomc/CursomApplication.java
@@ -1,13 +1,33 @@
 package com.arthurvinicius.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomApplication {
+import com.arthurvinicius.cursomc.domain.Filme;
+import com.arthurvinicius.cursomc.repositories.FilmeRepositorys;
 
+@SpringBootApplication
+public class CursomApplication implements CommandLineRunner {
+
+	@Autowired
+	private FilmeRepositorys filmeRepositorys;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Filme cat1 = new Filme(null, "Ação");
+		Filme cat2 = new Filme(null, "Aventura");
+	
+	filmeRepositorys.saveAll(Arrays.asList(cat1,cat2));
+		
+	}	
 
 }
